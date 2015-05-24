12755fb251405b2d3414923a7355a590b316bf40
Kevin Lesht <klesht@gmail.com>
2015-05-24 15:04:26 -0500

    Initial styles in place, beginning work on modules

--- index.php
+++ index.php
@@ -3,8 +3,92 @@
 <head>
 	<meta charset="UTF-8">
 	<title>SwiftIpsum</title>
+
+	<link rel="stylesheet" href="scss/global.min.css">
 </head>
 <body>
+
+	<header>
+
+		<div class="container">
+
+			<a href="#" class="logo">SwiftIpsum</a>
+
+			<!-- <span class="aux">The fastest way for designers and developers to fill their ideas with content.</span> -->
+
+		</div>
+
+	</header>
+
+	<main class="container">
+
+		<div class="column">
+			
+			<div class="module">
+				
+				<?php include ('modules/plaintext.php'); ?>
+
+			</div>
+
+			<div class="module">
+				
+				<?php include ('modules/paragraphs.php'); ?>
+
+			</div>
+
+			<div class="module">
+				
+				<?php include ('modules/headings.php'); ?>
+
+			</div>
+
+			<div class="module">
+				
+				<?php include ('modules/ul.php'); ?>
+
+			</div>
+
+			<div class="module">
+				
+				<?php include ('modules/ol.php'); ?>
+
+			</div>
+
+			<div class="module">
+				
+				<?php include ('modules/navigation.php'); ?>
+
+			</div>
+
+			<div class="module">
+				
+				<?php include ('modules/form.php'); ?>
+
+			</div>																		
+
+		</div>
+
+		<div class="column">
+
+		</div>
+
+		<div class="column">
+
+		</div>
+
+	</main>
+
+	<footer>
+
+		<div class="container">
+
+			<a href="#" class="logo">SwiftIpsum</a>
+
+			<a href="mailto:contact@swiftipsum.com"><span></span></a>
+
+		</div>
+
+	</footer>
 	
 </body>
 </html>
